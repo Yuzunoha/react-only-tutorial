ce41d263f700d653158917b2c2210729a675d762
Yuzunoha <studysteadystandby@gmail.com>
2020-11-25 23:32:30 +0900

    関数コンポーネント。アロー関数でもいけそう

--- main.jsx
+++ main.jsx
@@ -1,6 +1,21 @@
-const App = () => {
-  return <div>Hello React!!</div>;
-};
+/**
+関数コンポーネント。引数は1つで、propsと呼ばれる。
+`<Tweet content=”Hello”/>`と記述するとprops.contentにHelloが入る。
+関数コンポーネントはJSXをreturnする。
+JSXの中で{}の中身はJSとして評価される。
+*/
+function Tweet(props) {
+  return <div>{props.content}</div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Tweet content='Hello' />
+      <Tweet content='React!' />
+    </div>
+  );
+}
 
 const target = document.querySelector('#app');
 ReactDOM.render(<App />, target);
