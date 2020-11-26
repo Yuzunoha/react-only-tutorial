84bc54e3ffebe2f7a16d5494f5b3d84849e38c6a
Yuzunoha <studysteadystandby@gmail.com>
2020-11-26 09:36:39 +0900

    飽きた

--- main.jsx
+++ main.jsx
@@ -5,6 +5,19 @@
 JSXの中で{}の中身はJSとして評価される。
 */
 function Tweet(props) {
+  // Reactからこのコンポーネントの
+  // like値と、likeの値をセットするための関数を取り出す
+  // デフォルト値はfalseにする
+  const [liked, setLike] = React.useState(false);
+  const toggleLike = React.useCallback(() => {
+    return setLike(
+      (prev) => {
+        return !prev;
+      },
+      [setLike]
+    );
+  });
+
   return (
     <div className='tweet'>
       <div className='icon-container'>{props.icon}</div>
@@ -14,6 +27,9 @@ function Tweet(props) {
           <span className='account-name'>@{props.accountName}</span>
         </div>
         <div className='content'>{props.content}</div>
+        <div className='status-action'>
+          <span onClick={toggleLike}>{liked ? '❤️' : '♡'}</span>
+        </div>
       </div>
     </div>
   );
